323f54fb99ef0d447026b549cca161ffbeb049a2
stevensillsii <stevensills2@gmail.com>
2023-10-22 21:18:49 -0700

    trying to get the circle.js to work

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+//the list of required dependencies
+//this one is for the file system
 const fs = require('fs');
+//this one is for the inquirer which handles the user questions
 const inquirer = require('inquirer');
+
 const { circle, triangle, square } = require('./lib/shapes.js')
 const { createSVG } = require('./lib/svgGenerator.js'); // this is reaqireing the svgGenerator.js file to get the function from there
 
@@ -10,7 +14,7 @@ inquirer
       type: 'input',
       name: 'text',
       message: 'Enter up to three characters: ',
-      validate: (input) => input.length <= 3,
+      validate: (input) => input.length <= 3, //this makes it so the user cant input more than 3 characters
     },
     {
       type: 'input',
@@ -29,12 +33,13 @@ inquirer
       message: 'Enter the shape color (keyword or hex code): ',
     },
   ])
+  //this is the function that takes the content and makes an .svg file
   .then((answers) => {
     // Generate the SVG content
     const svgContent = createSVG(answers);
-
+  
     // Write the SVG content to a file
     fs.writeFileSync('logo.svg', svgContent);
-
-    console.log('Generated logo.svg');
+//generates a message "Generated logo.svg!" once the process is finished and the logo is made
+    console.log('Generated logo.svg!');
   });
