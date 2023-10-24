681efc7d6392e22c8ce7ee10fdbaac339d519792
stevensillsii <stevensills2@gmail.com>
2023-10-23 22:29:22 -0700

    all 3 Jest tests work

--- test/square.test.js
+++ test/square.test.js
@@ -1,16 +1,16 @@
 const Square = require('../lib/square.js'); 
 
 describe('Square', () => {
-  it('should render an SVG with the correct attributes', () => {
+  test('should render an SVG with the correct attributes', () => {
     const square = new Square();
     square.color = 'blue';
     square.textColor = 'white';
-    square.text = 'Test Square';
+    square.text = 'MAD';
 
     const expectedSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="500" height="500">
         <rect width="200" height="200" fill="blue" />
         <text x="100" y="110" font-size="65" text-anchor="middle" fill="white">
-          Test Square
+          MAD
         </text>
       </svg>`;
 
--- test/triangle.test.js
+++ test/triangle.test.js
@@ -1,2 +1,18 @@
 const Triangle = require("../lib/triangle.js");
 
+describe("Triangle", () => {
+  test("renders a triangle SVG correctly", () => {
+    const triangle = new Triangle("blue", "WOW", "red");
+    const svg = triangle.render();
+
+    // Define the expected SVG string
+    const expectedSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="500" height="500">
+        <polygon points="100, 15 200, 200 0, 200" fill="blue" />
+        <text x="100" y="185" font-size="70" text-anchor="middle" fill="red">
+          WOW
+        </text>
+      </svg>`;
+
+    expect(svg).toBe(expectedSVG);
+  });
+});
